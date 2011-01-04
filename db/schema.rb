c9543a4e24d581346835e4fbbba458409b448b32
Dave Doolin <david.doolin@gmail.com>
2011-01-04 13:30:23 -0800

    Added to the readme.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101217014603) do
+ActiveRecord::Schema.define(:version => 20110104201910) do
 
   create_table "tasks", :force => true do |t|
     t.string   "description"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(:version => 20101217014603) do
     t.datetime "updated_at"
     t.datetime "start_time"
     t.datetime "finish_time"
+    t.string   "tags"
   end
 
   create_table "users", :force => true do |t|
