071e40ce7163b15ffbb397ef4bfa48e8379fa99b
Dave Doolin <david.doolin@gmail.com>
2013-02-17 11:11:59 -0800

    updated to devise 2.0

--- db/migrate/20101217012805_devise_create_users.rb
+++ db/migrate/20101217012805_devise_create_users.rb
@@ -1,15 +1,28 @@
 class DeviseCreateUsers < ActiveRecord::Migration
   def self.up
     create_table(:users) do |t|
-      t.database_authenticatable :null => false
-      t.recoverable
-      t.rememberable
-      t.trackable
+
+      #t.database_authenticatable :null => false
+      t.string :email, :null => false, :default => ""
+      t.string :encrypted_password, :null => false, :default => ""
+
+      # recoverable
+      t.string :reset_password_token
+      t.string :reset_password_sent_at
+
+      # rememberable
+      t.datetime :remember_created_at
+
+      # trackable
+      t.integer :sign_in_count, :default => 0
+      t.datetime :current_sign_in_at
+      t.datetime :last_sign_in_at
+      t.string :current_sign_in_ip
+      t.string :last_sign_in_ip
 
       # t.confirmable
       # t.lockable :lock_strategy => :failed_attempts, :unlock_strategy => :both
       # t.token_authenticatable
-
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -23,34 +23,34 @@ ActiveRecord::Schema.define(:version => 20110802181853) do
     t.string   "linkedin"
     t.string   "google"
     t.string   "url"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "tasks", :force => true do |t|
     t.string   "description"
     t.boolean  "started"
     t.boolean  "finished"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
     t.datetime "start_time"
     t.datetime "finish_time"
     t.string   "tags"
   end
 
   create_table "users", :force => true do |t|
-    t.string   "email",                               :default => "", :null => false
-    t.string   "encrypted_password",   :limit => 128, :default => "", :null => false
+    t.string   "email",                  :default => "", :null => false
+    t.string   "encrypted_password",     :default => "", :null => false
     t.string   "reset_password_token"
-    t.string   "remember_token"
+    t.string   "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                       :default => 0
+    t.integer  "sign_in_count",          :default => 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
