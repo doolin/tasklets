d32474d3b6849fdb0616ac447504444f07a60e6f
Dave Doolin <david.doolin@gmail.com>
2013-05-17 19:39:27 -0700

    updated task model and spec

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe Task do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "doesn't save an invalid model" do
+    t = Task.create
+    t.should_not be_valid
+  end
+
+  it "saves a valid task" do
+    t = Task.create!(:description => "jgshdlfjk")
+    t.should be_valid
+  end
+
 end
