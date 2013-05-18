27e567b82aaad1833f56385148d3494a081bb5fe
Dave Doolin <david.doolin@gmail.com>
2013-05-17 17:14:27 -0700

    updated secret token to rails 4

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,3 +5,4 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 App1::Application.config.secret_token = '53da8ffe0d0e1c89d55ef9bf992fb2fe19f89b25bb4d78529ae1936f56c29f9e054a279714d4bc6928e158982d45b1ee28079477ec8a97d7fd9da198f48b3c27'
+App1::Application.config.secret_key_base = 'new secret key base'
