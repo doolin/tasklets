764fcbe734497f15555356e084cedc4adea578fc
Dave Doolin <david.doolin@gmail.com>
2015-08-10 05:17:16 -0700

    profiles routing to rspec 3

--- spec/routing/profiles_routing_spec.rb
+++ spec/routing/profiles_routing_spec.rb
@@ -4,31 +4,31 @@ describe ProfilesController do
   describe "routing" do
 
     it "routes to #index" do
-      get("/profiles").should route_to("profiles#index")
+      expect(get("/profiles")).to route_to("profiles#index")
     end
 
     it "routes to #new" do
-      get("/profiles/new").should route_to("profiles#new")
+      expect(get("/profiles/new")).to route_to("profiles#new")
     end
 
     it "routes to #show" do
-      get("/profiles/1").should route_to("profiles#show", :id => "1")
+      expect(get("/profiles/1")).to route_to("profiles#show", :id => "1")
     end
 
     it "routes to #edit" do
-      get("/profiles/1/edit").should route_to("profiles#edit", :id => "1")
+      expect(get("/profiles/1/edit")).to route_to("profiles#edit", :id => "1")
     end
 
     it "routes to #create" do
-      post("/profiles").should route_to("profiles#create")
+      expect(post("/profiles")).to route_to("profiles#create")
     end
 
     it "routes to #update" do
-      put("/profiles/1").should route_to("profiles#update", :id => "1")
+      expect(put("/profiles/1")).to route_to("profiles#update", :id => "1")
     end
 
     it "routes to #destroy" do
-      delete("/profiles/1").should route_to("profiles#destroy", :id => "1")
+      expect(delete("/profiles/1")).to route_to("profiles#destroy", :id => "1")
     end
 
   end
