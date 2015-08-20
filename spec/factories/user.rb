ec01cefda6d3ed7c0ed565ff4290b5cd65fdc66f
Dave Doolin <david.doolin@gmail.com>
2015-08-20 06:07:52 -0700

    rubocop

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,5 +1,5 @@
 FactoryGirl.define do
-  factory :minimal_user, :class => User do |u|
+  factory :minimal_user, :class => User do
     username 'minimal'
     email 'minimal@example.com'
     password 'test1234'
