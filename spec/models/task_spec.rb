35aa202d6c1141ea04ce74183ec3e6f60abbe229
Dave Doolin <david.doolin@gmail.com>
2015-09-14 03:41:09 -0700

    updated rspec

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -2,12 +2,10 @@ require 'spec_helper'
 
 describe Task do
   it "doesn't save an invalid model" do
-    t = Task.create
-    t.should_not be_valid
+    expect(Task.create).not_to be_valid
   end
 
   it 'saves a valid task' do
-    t = Task.create!(description: 'jgshdlfjk')
-    t.should be_valid
+    expect(Task.create!(description: 'jgshdlfjk')).to be_valid
   end
 end
