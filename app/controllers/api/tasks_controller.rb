72401f99900e89629675939e00e374f4cceecad2
Dave Doolin <david.doolin@gmail.com>
2017-09-29 04:51:37 -0700

    api create

--- app/controllers/api/tasks_controller.rb
+++ app/controllers/api/tasks_controller.rb
@@ -5,5 +5,14 @@ module Api
     def show
       render json: Task.first.to_json
     end
+
+    def create
+      current_user.tasks.build(permitted_params).save!
+      render json: {}, status: :created
+    end
+
+    def permitted_params
+      params.require(:task).permit(:description)
+    end
   end
 end
--- spec/controllers/api/tasks_controller_spec.rb
+++ spec/controllers/api/tasks_controller_spec.rb
@@ -3,14 +3,26 @@
 require 'rails_helper'
 
 RSpec.describe Api::TasksController do
+  let(:user) { create :user}
+  before { request.headers.merge! user.create_new_auth_token }
+
   describe '#show' do
     it 'shows' do
-      user = create :user
-      request.headers.merge! user.create_new_auth_token
+      # user = create :user
+      # request.headers.merge! user.create_new_auth_token
 
       task = Task.create! description: 'foobar', user: user
       get :show, params: { id: task.id }
       expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe '#create' do
+    it 'a task for signed in user' do
+      expect {
+        post :create, params: { task: { description: 'some task' } }
+        expect(response).to have_http_status(:created)
+      }.to change{Task.count}.by 1
     end
   end
 end
