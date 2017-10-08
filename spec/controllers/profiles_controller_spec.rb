f07890abd78ba5a0cc90848e6f594b47d0912ec7
Dave Doolin <david.doolin@gmail.com>
2017-10-08 09:21:33 -0700

    controller-testing - profiles controller specs
    This is a wrap up for this branch. There is more
    which could be done between controller and request
    specs, but this is enough for now.

--- spec/controllers/profiles_controller_spec.rb
+++ spec/controllers/profiles_controller_spec.rb
@@ -2,159 +2,110 @@
 
 require 'spec_helper'
 
-describe ProfilesController, type: :request do
-  # This should return the minimal set of attributes required to create a valid
-  # Profile. As you add validations to Profile, be sure to
-  # update the return value of this method accordingly.
-  #
-  # TODO: either update this, or preferably, delete it in favor of
-  # using FactoryGirl attributes_for.
-  def valid_attributes(attrs = {})
-    {
-      firstname: 'cersei'
-    }.merge(attrs)
-  end
-
+describe ProfilesController, type: :controller do
   let(:user) { User.create!(email: 'foo@bar.com', password: 'foobarski') }
 
   before { sign_in user }
 
-  describe 'GET index', type: :request do
-    it 'assigns all profiles as @profiles' do
-      Profile.create! valid_attributes(user: user)
-      # get :index
-      # expect(assigns(:profiles).first).to eq(profile)
-      get profiles_url
+  describe '#index' do
+    it 'valid request returns successfully' do
+      create :profile
+      get :index
       expect(response).to have_http_status(:ok)
     end
   end
 
-  describe 'GET show', type: :request do
-    it 'assigns the requested profile as @profile' do
-      profile = Profile.create! valid_attributes(user: user)
-      # get :show, params: { id: profile.id.to_s }
-      # expect(assigns(:profile)).to eq(profile)
-      get profile_url(profile.id) # , params: { id: profile.id.to_s }
+  describe '#show' do
+    it '' do
+      profile = create :profile
+      get :show, params: { id: profile.id.to_s }
       expect(response).to have_http_status(:ok)
     end
   end
 
-  describe 'GET new', type: :request do
-    it 'assigns a new profile as @profile' do
-      # get :new
-      # expect(assigns(:profile)).to be_a_new(Profile)
-      get new_profile_url
+  describe '#new' do
+    it '' do
+      get :new
       expect(response).to have_http_status(:ok)
     end
   end
 
-  describe 'GET edit', type: :request do
-    it 'assigns the requested profile as @profile' do
-      profile = Profile.create! valid_attributes(user: user)
-      # get :edit, params: { id: profile.id.to_s }
-      # expect(assigns(:profile)).to eq(profile)
-      get edit_profile_url(profile.id)
+  describe '#edit' do
+    it '' do
+      profile = create :profile
+      get :edit, params: { id: profile.id.to_s }
       expect(response).to have_http_status(:ok)
     end
   end
 
   describe '#create' do
+    let(:valid_attributes) do
+      { firstname: 'cersei' }
+    end
+
     context 'with valid params' do
       it 'creates a new Profile' do
         parameters = { profile: valid_attributes }
         expect do
-          # post :create, params: parameters
-          post profiles_url, params: parameters
+          post :create, params: parameters
           expect(response).to have_http_status(:redirect)
         end.to change(Profile, :count).by(1)
       end
 
       it 'redirects to the created profile' do
-        # post :create, params: { profile: valid_attributes }
-        post profiles_url, params: { profile: valid_attributes }
+        post :create, params: { profile: valid_attributes }
         expect(response).to redirect_to(Profile.last)
       end
     end
 
     context 'with invalid params' do
-      xit 'assigns a newly created but unsaved profile as @profile' do
+      it "redirects to new" do
         allow_any_instance_of(Profile).to receive(:save).and_return(false)
-        # post :create, params: { profile: { foo: :bar } }
-        # expect(assigns(:profile)).to be_a_new(Profile)
-        post profiles_url, params: { profile: { foo: :bar } }
-        expect(response).to have_http_status(:forbidden)
-      end
-
-      it "re-renders the 'new' template" do
-        allow_any_instance_of(Profile).to receive(:save).and_return(false)
-        # post :create, params: { profile: { foo: :bar } }
-        # expect(response).to render_template('new')
-        # post :create, params: { profile: { foo: :bar } }
-        post profiles_url, params: { profile: { foo: :bar } }
+         post :create, params: { profile: { foo: :bar } }
         expect(response).to redirect_to(new_profile_url)
       end
     end
   end
 
-  # TODO: refactor and general cleanup
-  # TODO: update profile factory to have all attributes and user association
-  describe '.update' do
+  describe '#update' do
     context 'with valid params' do
       it 'updates the requested profile' do
         profile = create :profile
-        # put :update, params: { id: profile.id, profile: { firstname: 'cersei' } }
-        put profile_url(profile.id), params: { profile: { firstname: 'cersei' } }
+        put :update, params: { id: profile.id, profile: { firstname: 'cersei' } }
         profile.reload
         expect(profile.firstname).to eq 'cersei'
       end
 
-      xit 'assigns the requested profile as @profile' do
-        profile = create :profile
-        put :update, params: { id: profile.id, profile: { lastname: 'lannister' } }
-        expect(assigns(:profile)).to eq(profile)
-      end
-
       it 'redirects to the profile' do
         profile = create :profile
-        # put :update, params: { id: profile.id, profile: { firstname: 'jaime', lastname: 'lannister' } }
-        put profile_url(profile.id), params: { profile: { firstname: 'jaime', lastname: 'lannister' } }
+        put :update, params: { id: profile.id, profile: { firstname: 'jaime', lastname: 'lannister' } }
         expect(response).to redirect_to(profile)
       end
     end
 
     context 'with invalid params' do
-      it 'assigns the profile as @profile' do
+      it '' do
         profile = create :profile
-        # put :update, params: { id: profile.id, profile: { foobar: 'quux' } }
-        # expect(assigns(:profile)).to eq(profile)
-        put profile_url(profile.id), params: { profile: { firstname: 'q' } }
-        expect(response).to have_http_status(:unprocessable_entity)
-      end
-
-      xit "re-renders the 'edit' template" do
-        profile = create :profile
-        put :update, params: { id: profile.id, profile: { quux: 'foobar' } }
-        expect(response).to render_template('edit')
+        put :update, params: { id: profile.id, profile: { foobar: 'quux' } }
+        aggregate_failures do
+          expect(response).to have_http_status(:redirect)
+          expect(response).to redirect_to(profile_url)
+        end
       end
     end
   end
 
-  describe 'DELETE destroy', type: :request do
-    let(:user) { create :user }
-    before { sign_in user }
-
+  describe '#destroy' do
     it 'destroys the requested profile' do
-      profile = Profile.create! valid_attributes(user: user)
+      profile = create :profile
       expect do
-        # delete :destroy, params: { id: profile.id.to_s }
-        delete profile_url(profile.id) # , params: { id: profile.id.to_s }
+        delete :destroy, params: { id: profile.id.to_s }
       end.to change(Profile, :count).by(-1)
     end
 
     it 'redirects to the profiles list' do
-      profile = Profile.create! valid_attributes(user: user)
-      # delete :destroy, params: { id: profile.id.to_s }
-      delete profile_url(profile.id)
+      profile = create :profile
+      delete :destroy, params: { id: profile.id.to_s }
       expect(response).to redirect_to(profiles_url)
     end
   end
