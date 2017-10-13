8ee4f6c3befa1a9246c3c965ebb701cdd0024358
Dave Doolin <david.doolin@gmail.com>
2017-10-13 12:13:16 -0700

    devise-token-auth - profile and user request spec
    This is the wrap up for devise-token-auth branch. The
    api calls are not complete, but there is enough working
    code to move forward on an api client branch.

---
+++ spec/requests/api/v1/profiles_request_spec.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'api_v1_profile request', type: :request do
+  let!(:user) { create :user }
+  let!(:header) { user.create_new_auth_token }
+
+  describe '#index' do
+  end
+
+  describe 'api_v1_profiles_url' do
+    example 'profile for signed in user' do
+      parameters = {
+        profile: {
+          firstname: 'fooish'
+        }
+      }
+      expect do
+        post api_v1_profiles_url, params: parameters, headers: header
+        expect(response).to have_http_status(:created)
+      end.to change { Profile.count }.by 1
+    end
+  end
+
+  describe 'api_v1_profile_url(profile)' do
+    example 'profile found by profile id' do
+      profile = Profile.create! firstname: 'foobar', user: user
+      get api_v1_profile_url(profile), headers: header
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe '#edit' do
+  end
+
+  describe '#update' do
+  end
+
+  describe '#destroy' do
+  end
+end
---
+++ spec/requests/api/v1/users_request_spec.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+module Api
+  module V1
+    RSpec.describe 'api_v1_users request', type: :request do
+      # The bang here forces evaluation right away, rather than
+      # punting it to within the expectation block, which throw
+      # off the count.
+      let!(:user) { create :user }
+      let(:headers) { user.create_new_auth_token }
+
+      describe '#index' do
+      end
+
+      describe 'api_v1_users_url' do
+        it 'creates new user with signed in user' do
+          parameters = {
+            user: {
+              email: 'foo@bar.com',
+              password: 'foobarski',
+              password_confirmation: 'foobarski'
+            }
+          }
+          expect do
+            post api_v1_users_url, params: parameters, headers: headers
+            expect(response).to have_http_status(:created)
+          end.to change { User.count }.by 1
+        end
+      end
+
+      describe 'api_v1_user_url(user)' do
+        it 'shows user found by user id' do
+          get api_v1_user_url(user), headers: headers
+          expect(response).to have_http_status(:ok)
+        end
+      end
+
+      describe '#edit' do
+      end
+
+      describe '#update' do
+      end
+
+      describe '#destroy' do
+      end
+    end
+  end
+end
