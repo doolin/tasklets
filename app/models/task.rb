55729667d0fad413c5a2600e5fc22d45db00c983
Dave Doolin <david.doolin@gmail.com>
2020-09-01 06:31:01 -0700

    TASKLETS-10 count descendants with CTE SQL query
    Rails doesn't support CTE queries, so it's necessary
    to construct CTEs using SQL then executing the query
    via a database connection.
    
    The query returns a tuple of hashes, which isn't terribly
    useful if one is interested in either the actual models,
    or representing the nested structure.
    
    It's trivial to count the results which is what this
    change demonstrates.

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,8 +5,30 @@ class Task < ActiveRecord::Base
 
   # https://guides.rubyonrails.org/association_basics.html#self-joins
   # to create a tree structure.
-  has_many :children, class_name: "Task", foreign_key: "parent_id"
-  belongs_to :parent, class_name: "Task", optional: true
+  has_many :children, class_name: 'Task', foreign_key: 'parent_id'
+  belongs_to :parent, class_name: 'Task', optional: true
 
   validates :description, presence: true
+
+  def self.count_descendents_with_cte(id = nil)
+    descendents_with_cte(id)[0]['count']
+  end
+
+  def self.descendents_with_cte(id)
+    id = id.nil? ? 'IS NULL' : "= #{id}"
+
+    # This pulls all the descendents into a flat array.
+    sql = <<-SQL
+      WITH RECURSIVE tree AS (
+        select t.id, t.parent_id, t.tags from tasks t where parent_id #{id}
+        UNION ALL
+        select t1.id, t1.parent_id, t1.tags from tree
+        join tasks t1 ON t1.parent_id = tree.id
+      ) SELECT count(*) FROM tree
+    SQL
+
+    # connection is in the ConnectionHandling module which is included in Base:
+    # https://github.com/rails/rails/blob/master/activerecord/lib/active_record/base.rb#L271
+    ActiveRecord::Base.connection.execute(sql)
+  end
 end
