51ffbe65b951de1e201dffc30121240e7864d99c
Dave Doolin <david.doolin@gmail.com>
2020-09-19 05:31:07 -0700

    TASKLETS-41 hook up rubocop to semaphoreci
    This change  updates the various rubocop gems, and fixes a few
    preexisting violations. Normally, violations would get separate commits
    to allow for easier revert if necessary, but the context here allows
    "bending the rules" a bit.

--- app/validators/task_label_validator.rb
+++ app/validators/task_label_validator.rb
@@ -1,3 +1,11 @@
+# frozen_string_literal: true
+
+# Tasks need a way to be identified regardless of their
+# position in a particular tree. Specifically, a table may
+# contain multiple "trees", that is, tasks with parent_id
+# NULL. Acquiring a task tree for a particular root node
+# requires selecting that root node correctly, for which
+# the label is used.
 class TaskLabelValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     record.errors.add(attribute, :on_label) unless value && value.length < 64
