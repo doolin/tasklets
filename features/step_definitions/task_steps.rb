5403cd48a933d9f5151cf4610587885773cb0ca6
Dave Doolin <david.doolin@gmail.com>
2020-09-20 05:06:20 -0700

    TASKLETS-59 fix cucumber feature/task failure
    When label was made a required attribute for a task,
    the corresponding for was not updated, inducing a
    cucumber failure on that form.
    
    A great argument for keeping cucumber in play.

--- features/step_definitions/task_steps.rb
+++ features/step_definitions/task_steps.rb
@@ -24,10 +24,10 @@ Given /^I fill in the "([^"]*)" with "([^"]*)"$/ do |field, value|
   fill_in(field, with: value)
 end
 
-When(/^I pressem button "(.*?)"$/) do |arg1|
+When(/^I press button "(.*?)"$/) do |arg1|
   click_button 'Create Task'
 end
 
-Then(/^I'ma get "(.*?)"$/) do |arg1|
+Then(/^I see message "(.*?)"$/) do |arg1|
   page.body.should have_selector '.flash.success', text: 'Task was successfully created.'
 end
